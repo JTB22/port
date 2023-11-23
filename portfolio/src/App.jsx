b356c45f60bf5932670d72d8f4e3654a236fa9d2
Josh B <115614487+JTB22@users.noreply.github.com>
2023-11-22 22:34:01 -0500

    layout started

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,31 +1,35 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/logo1.png";
+// import githubLogo from "/icons8-github.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="App">
       <div className="header">
-        <a href="https://joshblood.com" target="_blank">
+        <h1>Joshua</h1>
+        <a href="https://joshblood.com" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Josh Blood" />
         </a>
-        <h1>Josh Blood</h1>
+        <h1>Blood</h1>
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="body">
+        <div className="card"></div>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
